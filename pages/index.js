b81de8ce6c4f77d7e07387284973668cf03f4414
Natalia Ignateva <ignatyevanat@gmail.com>
2021-07-27 09:55:31 +0200

    Create the form

--- pages/index.js
+++ pages/index.js
@@ -1,77 +1,79 @@
 import Head from 'next/head';
 import Parser from 'rss-parser';
+import Link from 'next/link';
 
 const Home = (props) => (
-  <div>
-    <Head>
-      <title>Latest posts</title>
-    </Head>
     <div>
-      <header>
-        <h1>Latest posts</h1>
-      </header>
-      <main>
-        <table>
-          <thead>
-            <tr>
-              <th>Post</th>
-              <th>Date</th>
-            </tr>
-          </thead>
-          <tbody>
-            {props.posts
-              //.sort((a, b) => new Date(b.date) - new Date(a.date))
-              .map((value, index) => {
-                return (
-                  <tr key={index}>
-                    <td>
-                      <div>
-                        <a href={value.link}>{value.title}</a>
-                      </div>
-                      <div>{value.name}</div>
-                    </td>
-                    <td>{new Date(value.date).toDateString()}</td>
-                  </tr>
-                );
-              })}
-          </tbody>
-        </table>
-      </main>
+        <Head>
+            <title>Latest posts</title>
+        </Head>
+        <div>
+            <header>
+                <h1>Latest posts</h1>
+                <p>
+                    <Link href="/form">
+                        <a>Add a new blog</a>
+                    </Link>
+                </p>
+            </header>
+            <main>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Post</th>
+                            <th>Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {props.posts
+                            //.sort((a, b) => new Date(b.date) - new Date(a.date))
+                            .map((value, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td>
+                                            <div>
+                                                <a href={value.link}>
+                                                    {value.title}
+                                                </a>
+                                            </div>
+                                            <div>{value.name}</div>
+                                        </td>
+                                        <td>
+                                            {new Date(
+                                                value.date,
+                                            ).toDateString()}
+                                        </td>
+                                    </tr>
+                                );
+                            })}
+                    </tbody>
+                </table>
+            </main>
+        </div>
     </div>
-  </div>
 );
 
 export async function getStaticProps(context) {
-  const parser = new Parser();
+    const parser = new Parser();
 
-  const feed = await parser.parseURL('https://teacherluke.co.uk/feed/');
+    const feed = await parser.parseURL('https://teacherluke.co.uk/feed/');
 
-  const posts = feed.items.reduce(
-    (acc, item) =>
-      acc.concat({
-        title: item.title,
-        link: item.link,
-        date: item.pubDate,
-        name: 'Luke Thompson',
-      }),
-    [],
-  );
+    const posts = feed.items.reduce(
+        (acc, item) =>
+            acc.concat({
+                title: item.title,
+                link: item.link,
+                date: item.pubDate,
+                name: 'Luke Thompson',
+            }),
+        [],
+    );
 
-  /*const posts = [];
-
-  feed.items.forEach((item) => {
-    posts.push({
-      title: item.title,
-      link: item.link,
-      date: item.pubDate,
-      name: 'Luke Thompson',
-    });
-  });*/
-  return {
-    props: {
-      posts,
-    },
-  };
+    return {
+        props: {
+            posts,
+        },
+    };
 }
 
 export default Home;
